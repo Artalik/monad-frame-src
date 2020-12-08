d74a9d10bfd2796b504796e664915779ca0abceb
Pierre-Évariste Dagand <pierre-evariste.dagand@lip6.fr>
2020-12-08 17:00:50 +0100

    WIP: move burden of proof to Coq, no Iris

--- relabel/relabel.v
+++ relabel/relabel.v
@@ -1,6 +1,6 @@
 Require Import MoSel.
 
-Import adequacy_gensym_SLMin.
+Import adequacy_gensym_SLList.
 
 Inductive Tree (X : Type) :=
 | Leaf : X -> Tree X
@@ -44,58 +44,115 @@ Fixpoint flatten {X} (t: Tree X) : list X :=
   | Node l r => flatten l ++ flatten r
   end.
 
-Definition sameShape {X Y} (t1 : Tree X) (t2 : Tree Y) : Prop :=
+Fixpoint sameShape {X Y} (t1 : Tree X) (t2 : Tree Y) : Prop :=
   match t1,t2 with
   | Leaf _, Leaf _ => True
-  | Node _ _, Node _ _  => True
+  | Node l1 r1, Node l2 r2  => sameShape l1 l2 /\ sameShape r1 r2
   | _, _ => False
   end.
 
-Lemma labelpost_fold : forall X (t : Tree X) t',
-    ⊢ labelPost t t' -∗ [∗ list] x ∈ (flatten t'), IsFresh x.
+Lemma finish_him2 X : forall (t : Tree X) t' h,
+    labelPost t t' () h -> NoDup h (* /\ sameShape t t' *).
 Proof.
-  induction t, t'; simpl; iIntros; norm_all; auto; try contradiction.
-  iDestruct (IHt1 with "HB") as "Ht1". iDestruct (IHt2 with "HC") as "Ht2".
-  iApply big_sepL_app. iFrame.
+induction t; intros t' h; destruct t'; simpl.
+- unfold hsingle=> ->. constructor. intro. inversion H. constructor.
+- MonPred.unseal; contradiction.
+- MonPred.unseal; contradiction.
+- MonPred.unseal => [-H].
+  repeat red in H.
+  destruct H as [h1 [h2 [P [Q [disj Heq]]]]].
+  specialize (IHt1 t'1 h1 P).
+  specialize (IHt2 t'2 h2 Q).
+  apply list_eq_comm in Heq.
+  eapply prop_eq; eauto. clear Heq.
+  apply NoDup_app; repeat split; auto.
+  intros * H1 H2.
+  unfold list_disjoint in disj.
+  apply elem_of_list_In in H1.
+  apply elem_of_list_In in H2.
+  eapply disj; eauto.
 Qed.
 
-Lemma sep_list_fold : forall a l, ⊢ (IsFresh a -∗ ([∗ list] x ∈ l, IsFresh x) -∗ ⌜a ∉ l⌝ : iProp).
-Proof.
-  induction l; iIntros "* HA HB".
-  - iPureIntro. intro. inversion H.
-  - iDestruct "HB" as "[HA' HB]".
-    iDestruct (singleton_neq with "HA HA'") as "%".
-    iDestruct (IHl with "HA HB") as "%". iPureIntro.
-    intro. inversion H1; subst; auto.
-Qed.
-
-Lemma fold_nodup : forall l,
-    ⊢ (([∗ list] x ∈ l, IsFresh x) : iProp) -∗ ⌜NoDup l⌝.
-Proof.
-  induction l; simpl; iIntros; norm_all; auto.
-  - iPureIntro. apply NoDup_nil_2.
-  - iDestruct (IHl with "HC") as "%". iDestruct (sep_list_fold with "HB HC") as "%".
-    iPureIntro. constructor; auto.
-Qed.
-
-Lemma labelpost_nodup : forall X (t : Tree X) t0,
-    ⊢ labelPost t t0 -∗ ⌜NoDup (flatten t0)⌝.
-Proof.
-  iIntros; norm_all. iDestruct (labelpost_fold with "HB") as "HB".
-  iDestruct (fold_nodup with "HB") as "$".
-Qed.
-
-Lemma finish_him : forall X (t : Tree X) t' h,
+Lemma finish_him2b X : forall (t : Tree X) t' h,
     labelPost t t' () h -> NoDup (flatten t') /\ sameShape t t'.
 Proof.
-  intros. apply soundness3 in H. apply (soundness1 h).
-  iIntros "HA". iDestruct (H with "HA") as "HA". iDestruct (labelpost_nodup with "HA") as "%".
-  iSplit; auto. destruct t,t'; auto.
+induction t; intros t' h; destruct t'; simpl.
+- unfold hsingle=> _. split; auto.
+  apply NoDup_singleton.
+- MonPred.unseal; contradiction.
+- MonPred.unseal; contradiction.
+- MonPred.unseal => [-H].
+  repeat red in H.
+  destruct H as [h1 [h2 [P [Q [disj Heq]]]]].
+  destruct (IHt1 t'1 h1 P) as [H11 H12].
+  destruct (IHt2 t'2 h2 Q) as [H21 H22].
+  apply list_eq_comm in Heq.
+  eapply prop_eq; eauto. clear Heq.
+  repeat split; auto.
+  apply NoDup_app; repeat split; auto.
+  intros * H1 H2.
+  unfold list_disjoint in disj.
+  apply elem_of_list_In in H1.
+  apply elem_of_list_In in H2.
+  eapply disj; eauto.
+Abort. (* NOT PROVABLE AS-IS! *)
+
+Lemma finish_him3 X : forall t' (t : Tree X) h,
+    labelPost t t' () h -> flatten t' = h.
+Proof.
+induction t'; intros t h; destruct t; simpl.
+- unfold hsingle=> ->. done.
+- MonPred.unseal; contradiction.
+- MonPred.unseal; contradiction.
+- MonPred.unseal => [-H].
+  repeat red in H.
+  destruct H as [h1 [h2 [P [Q [disj Heq]]]]].
+  rewrite (IHt'1 t1 h1 P).
+  rewrite (IHt'2 t2 h2 Q).
+  apply list_eq_comm in Heq.
+  eapply prop_eq; eauto.
+Qed.
+
+Lemma finish_him4 X : forall (t : Tree X) t' h,
+    labelPost t t' () h -> h = flatten t' /\ NoDup h /\ sameShape t t'.
+Proof.
+induction t; intros t' h; destruct t'; simpl.
+- unfold hsingle=> ->. repeat split; auto.
+  apply NoDup_singleton.
+- MonPred.unseal; contradiction.
+- MonPred.unseal; contradiction.
+- MonPred.unseal => [-H].
+  repeat red in H.
+  destruct H as [h1 [h2 [P [Q [disj Heq]]]]].
+  eapply list_eq_comm in Heq.
+  pattern h.
+  eapply prop_eq; eauto.
+  destruct (IHt1 t'1 h1 P) as [-> [H12 H13]].
+  destruct (IHt2 t'2 h2 Q) as [-> [H22 H23]].
+  repeat split; auto.
+  apply NoDup_app; repeat split; auto.
+  intros * H1 H2.
+  unfold list_disjoint in disj.
+  apply elem_of_list_In in H1.
+  apply elem_of_list_In in H2.
+  eapply disj; eauto.
+Qed.
+
+Lemma finish_him4w X : forall (t : Tree X) t' h,
+    labelPost t t' () h -> NoDup (flatten t') /\ sameShape t t'.
+Proof.
+intros.
+apply finish_him4 in H as [-> ?]. auto.
 Qed.
 
 Lemma relabel_spec : forall X (t : Tree X) t',
     relabel t = t' -> NoDup (flatten t') /\ sameShape t t'.
 Proof.
-  intros. unfold relabel in H. destruct (run (label t) initial_state) eqn:?. destruct H. simpl.
-  eapply (finish_him _ _ _ _ (adequacy_triple_init _ _ _ _ _ emp_trivial (label_spec _ t) Heqp)).
+  intros. 
+  unfold relabel in H. destruct (run (label t) initial_state) eqn:?. 
+  destruct H. simpl.
+  eapply finish_him4w. (* XXX: this suggests that [adequacy_triple_init] is too specific *)
+  eapply adequacy_triple_init; eauto.
+  by apply emp_trivial.
+  by apply label_spec.
 Qed.
